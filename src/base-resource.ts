62055b737fb147d15c7a93d330eba84a29d3d198
Mustafa Şentürk <mustafa.senturk@ode.al>
2026-06-23 08:31:50 +0000

    Release SDK version 2.17.7

--- src/base-resource.ts
+++ src/base-resource.ts
@@ -52,7 +52,7 @@ export abstract class BaseResource {
   
     protected readonly log: OdealLogger;
     
-  private readonly AGENT = "OdealSdkTypeScriptClient/2.17.6";
+  private readonly AGENT = "OdealSdkTypeScriptClient/2.17.7";
   
     private readonly circuitBreaker?: OdealCircuitBreaker;
     
